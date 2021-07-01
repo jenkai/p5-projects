0300cb2291d30f152a7a6ad011c583f512a6cfdd
Jen Kai You <youjenkai@gmail.com>
2021-07-01 22:13:30 +0800

    This is fifth version

--- github_test/sketch.js
+++ github_test/sketch.js
@@ -10,4 +10,5 @@ function draw() {
 	text('This is first version', width/2, height/2);
 	text('This is second version', width/2, height/2+20);
 	text('This is third version', width/2, height/2+40);
+	text('This is fifth version', width/2, height/2+60);
 }
