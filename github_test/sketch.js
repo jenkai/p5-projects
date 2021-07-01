10d79d484bf638aca31a00bc4c67837176b68f01
Jen Kai You <youjenkai@gmail.com>
2021-07-01 20:54:58 +0800

    init

---
+++ github_test/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	background(100)
+}
+
+function draw() {
+	textAlign(CENTER)
+	fill(255)
+	textSize(20)
+	text('This is first version', width/2, height/2);
+}
