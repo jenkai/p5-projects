389195d669a2b32e16b87b0996c279cf7a71e4aa
Jen Kai You <youjenkai@gmail.com>
2021-07-02 16:37:45 +0800

    multple waves

--- sine_wave/sketch.js
+++ sine_wave/sketch.js
@@ -1,26 +1,32 @@
-let pos = []
-let r = 200
-let num = 30
+class Wave {
+	constructor(amp, per, pha) {
+		this.amplitude = amp
+		this.period = per
+		this.phase = pha
+	}
+	caculate(x) {
+		return (sin(this.phase + TWO_PI * x / this.period) * this.amplitude)
+	}
+}
 
+let wave = []
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	for (let x = 0; x < num; x++) {
-		pos[x] = map(x, 0, num, 0, 2 * TWO_PI); //這句很精華
-		
+	for (let i = 0; i < 10; i++) {
+		wave[i] = new Wave(random(100,300), random(200,800), random(TWO_PI))
 	}
+	
 }
-let inc = 1
 
 function draw() {
 	background(0);
-	stroke(255)
-	translate(50, height / 2);
-	for (let x = 0; x < num; x++) {
-		
-		circle((width/num)*x, sin(pos[x] + inc) * r,20)
-		line((width/num)*x,0,(width/num)*x, sin(pos[x] + inc) * r,5)
+	for (let x = 0; x < width; x += 10) {
+		for (const iterator of wave) {
+			noStroke()
+			let y = iterator.caculate(x)
+			ellipse(x, y + height / 2, 10)
+		}
 		
 	}
-	inc += 0.1
 }
