566d2aec29eddf60a30f269b09e3178066e1012e
Jen Kai You <youjenkai@gmail.com>
2021-07-02 16:03:24 +0800

    normal sin wave use pos-shift

--- sine_wave/sketch.js
+++ sine_wave/sketch.js
@@ -1,42 +1,26 @@
+let pos = []
+let r = 200
+let num = 30
+
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	// put setup code here
+	for (let x = 0; x < num; x++) {
+		pos[x] = map(x, 0, num, 0, 2 * TWO_PI); //這句很精華
+		
+	}
 }
-
-let r = 100
-let inc = 0
-let y = 0
-let xoff = 0
+let inc = 1
 
 function draw() {
-	//normal
-
-	translate(0, height / 2)
-	// y = sin(inc) * r
-	// strokeWeight(3)
-	// stroke(0)
-	// point(inc*10, y)
-	// inc += 0.1
-
-	//advanced運用每次的xoff讓y變化使人以為畫布像右
-
-	background(255)
-	fill(100)
-	noStroke()
-	rect(0,0,width,height)
-	strokeWeight(2)
-	for (let i = 0; i < width-500; i++) {
-		y =  sin(i*0.1+xoff)*r
-		if (y>0) {
-			stroke(255)
-			point(i,y)
-		} else {
-			stroke(0)
-			point(i,y)
-		}
+	background(0);
+	stroke(255)
+	translate(50, height / 2);
+	for (let x = 0; x < num; x++) {
+		
+		circle((width/num)*x, sin(pos[x] + inc) * r,20)
+		line((width/num)*x,0,(width/num)*x, sin(pos[x] + inc) * r,5)
+		
 	}
-	strokeWeight(5)
-	point(width-500, sin(500*0.1+xoff)*r)
-	xoff +=0.1
-
+	inc += 0.1
 }
