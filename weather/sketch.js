c8c3f53c4ddae79a69ec029954e9aec9788162b2
Jen Kai You <youjenkai@gmail.com>
2021-07-05 15:11:01 +0800

    weather ver2 add animne

--- weather/sketch.js
+++ weather/sketch.js
@@ -2,18 +2,22 @@ let sunRise
 let sunSet
 let today
 let lenth = 500
-let pos = []
+
+function preload() {
+	loadJSON("https://opendata.cwb.gov.tw/api/v1/rest/datastore/A-B0062-001?Authorization=CWB-F739ECEB-698F-4DA2-BB8E-D5A590A7FEFD&format=JSON&locationName=%E5%AE%9C%E8%98%AD%E7%B8%A3&timeFrom=2021-07-05&timeTo=2021-07-06&sort=", analyze)
+}
+
 
 function analyze(data) {
 	sunRise = data.records.locations.location[0].time[0].parameter[1].parameterValue
+	sunMid = data.records.locations.location[0].time[0].parameter[3].parameterValue
 	sunSet = data.records.locations.location[0].time[0].parameter[5].parameterValue
-	console.log(sunRise)
 }
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(100)
-	loadJSON("https://opendata.cwb.gov.tw/api/v1/rest/datastore/A-B0062-001?Authorization=CWB-F739ECEB-698F-4DA2-BB8E-D5A590A7FEFD&format=JSON&locationName=%E5%AE%9C%E8%98%AD%E7%B8%A3&timeFrom=2021-07-05&timeTo=2021-07-06&sort=", analyze)
+	lenth = width/2
 }
 
 
@@ -26,6 +30,7 @@ function draw() {
 	let y2 = 0
 	let y3 = -lenth / 2 - 20
 	let y4 = -lenth / 2 - 20
+	let pos = []
 	today = new Date();
 	background(200)
 	translate(width / 2, height / 1.5)
@@ -37,8 +42,8 @@ function draw() {
 	bezier(x1, y1, x2, y2, x3, y3, x4, y4);
 
 
-	let steps = 10;
-	for (let i = 0; i <= steps; i++) {
+	let steps = 100;
+	for (let i = 0; i < steps; i++) {
 		let t = i / steps;
 		let x = bezierPoint(x1, x2, x3, x4, t);
 		let y = bezierPoint(y1, y2, y3, y4, t);
@@ -51,21 +56,29 @@ function draw() {
 	bezier(-x1, y1, -x2, y2, -x3, y3, -x4, y4);
 
 
-	for (let i = 0; i <= steps; i++) {
+	for (let i = 0; i < steps; i++) {
 		let ta = i / steps;
-		let xa = bezierPoint(-x1, -x2, -x3, -x4, ta);
-		let ya = bezierPoint(y1, y2, y3, y4, ta);
+		let xa = bezierPoint(x4, -x3, -x2, -x1, ta);
+		let ya = bezierPoint(y4, y3, y2, y1, ta);
 		pos.push([xa, ya])
 	}
 
+	let sunRiseTime = parseInt(sunRise.substr(0, 2)) * 60 + parseInt(sunRise.substr(3, 2))
+	let sunSetTime = parseInt(sunSet.substr(0, 2)) * 60 + parseInt(sunSet.substr(3, 2))
+
+
+	let dayLenth = sunSetTime - sunRiseTime
+	let currentT = floor(map((abs((today.getHours() * 60 + today.getMinutes()) - sunRiseTime)), 0, dayLenth, 0, steps * 2))
+
+
 	noStroke()
 	fill(250, 250, 0)
-	ellipse(pos[7][0], pos[7][1], 20, 20)
+	ellipse(pos[currentT][0], pos[currentT][1], 20, 20)
 
 	showText()
 }
 
-function showText(){
+function showText() {
 
 	fill(255)
 	noStroke()
@@ -74,11 +87,7 @@ function showText(){
 	text(today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds(), 0, 100)
 
 
-	fill(255, 80)
-	strokeWeight(0)
-	rect(-lenth / 2, -lenth / 5, (lenth / 2) * 2, lenth / 5, 10, 10, 0, 0)
-	
-	
+
 	fill(255)
 	textAlign(LEFT);
 	textFont('Optima')
