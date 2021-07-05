c6302433dd44ff9018b94e0f741d2c51703ee8f2
Jen Kai You <youjenkai@gmail.com>
2021-07-05 21:18:56 +0800

    weather update add moonamd dark mode

--- weather/sketch.js
+++ weather/sketch.js
@@ -2,8 +2,11 @@ let sunRise
 let sunSet
 let today
 let lenth = 500
-
+let bgCol
+let textBW 
+let moon
 function preload() {
+	moon = loadImage('moon.png')
 	loadJSON("https://opendata.cwb.gov.tw/api/v1/rest/datastore/A-B0062-001?Authorization=CWB-F739ECEB-698F-4DA2-BB8E-D5A590A7FEFD&format=JSON&locationName=%E5%AE%9C%E8%98%AD%E7%B8%A3&timeFrom=2021-07-05&timeTo=2021-07-06&sort=", analyze)
 }
 
@@ -16,7 +19,7 @@ function analyze(data) {
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(100)
+	background(0, 0)
 	lenth = width/2
 }
 
@@ -28,21 +31,41 @@ function draw() {
 	let x4 = 0
 	let y1 = 0
 	let y2 = 0
-	let y3 = -lenth / 2 
-	let y4 = -lenth / 2 
+	let y3 = -lenth / 2
+	let y4 = -lenth / 2
 	let pos = []
+	let steps = 100;
 	today = new Date();
-	background(200)
-	translate(width / 2, height /1.7)
+	clear()
+	translate(width / 2, height / 1.7)
 
 
+
+
+	let sunRiseTime = parseInt(sunRise.substr(0, 2)) * 60 + parseInt(sunRise.substr(3, 2))
+	let sunSetTime = parseInt(sunSet.substr(0, 2)) * 60 + parseInt(sunSet.substr(3, 2))
+
+
+	let dayLenth = sunSetTime - sunRiseTime
+	let currentT = floor(map((abs((today.getHours() * 60 + today.getMinutes()) - sunRiseTime)), 0, dayLenth, 0, steps * 2))
+	if (currentT < 100) {
+		bgCol = map(currentT, 0, 100, 50, 255)
+
+	} else {
+		bgCol = map(currentT, 100, 200, 255, 50)
+	}
+	if (bgCol < 150) {
+		textBW = 255
+	} else {
+		textBW = 0
+	}
 	noFill()
 	strokeWeight(2)
-	stroke(0)
+	stroke(textBW)
 	bezier(x1, y1, x2, y2, x3, y3, x4, y4);
 
 
-	let steps = 100;
+	
 	for (let i = 0; i < steps; i++) {
 		let t = i / steps;
 		let x = bezierPoint(x1, x2, x3, x4, t);
@@ -52,7 +75,7 @@ function draw() {
 
 	noFill()
 	strokeWeight(2)
-	stroke(0)
+	stroke(textBW)
 	bezier(-x1, y1, -x2, y2, -x3, y3, -x4, y4);
 
 
@@ -63,37 +86,49 @@ function draw() {
 		pos.push([xa, ya])
 	}
 
-	let sunRiseTime = parseInt(sunRise.substr(0, 2)) * 60 + parseInt(sunRise.substr(3, 2))
-	let sunSetTime = parseInt(sunSet.substr(0, 2)) * 60 + parseInt(sunSet.substr(3, 2))
-
-
-	let dayLenth = sunSetTime - sunRiseTime
-	let currentT = floor(map((abs((today.getHours() * 60 + today.getMinutes()) - sunRiseTime)), 0, dayLenth, 0, steps * 2))
-
-
+	
+	let c = select('body')
+	c.style('background-color', 'rgb(' + str(bgCol) + ',' + str(bgCol) + ',' + str(bgCol) + ')')
+	c.style('color', 'rgb(' + str(textBW) + ',' + str(textBW) + ',' + str(textBW) + ')')
+	showText()
 	
 	fill(250, 250, 0)
-	ellipse(pos[currentT][0], pos[currentT][1], 30, 30)
+	if(today.getHours() * 60 + today.getMinutes()<sunSetTime){
+		ellipse(pos[currentT][0], pos[currentT][1], 30, 30)
+	}
+	else{
+		image(moon,lenth/2,-lenth/1.4,100,100)
+	}
+	
+	
 
-	showText()
+
 }
 
 function showText() {
 
-	fill(0)
+	fill(textBW)
 	textFont('Josefin Sans')
 	noStroke()
-	textAlign(CENTER,TOP)
+	textAlign(CENTER, TOP)
 	textStyle(BOLD)
 	textStyle()
 	textSize(30)
-	text(today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds(), 0, 40)
+
+	if (today.getSeconds() < 10 && today.getMinutes() < 10) {
+		text(today.getHours() + ':0' + today.getMinutes() + ':0' + today.getSeconds(), 0, 40)
+	} else if (today.getSeconds() < 10) {
+		text(today.getHours() + ':' + today.getMinutes() + ':0' + today.getSeconds(), 0, 40)
+	} else {
+		text(today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds(), 0, 40)
+	}
 
 
 
-	fill(0)
+
+	fill(textBW)
 	textAlign(LEFT);
-	textSize(20)
+	textSize(25)
 	text(sunRise, -lenth / 2, 40)
 
 
